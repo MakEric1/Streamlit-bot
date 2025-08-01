dcf50b319b0b90dfdda1919e07641592bebc9f44
MakEric1 <g77x77@gmail.com>
2025-08-01 16:20:42 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -15,7 +15,7 @@ def response_generator():
         yield word + " "
         time.sleep(0.05)
 
-st.title("Simple chat")
+st.title("Simple chatbot - Mohamed Shehata")
 
 # Initialize chat history
 if "messages" not in st.session_state:
